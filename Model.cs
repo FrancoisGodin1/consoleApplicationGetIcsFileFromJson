e93b326fd9e270b165cb0ffadf88705ef4156a72
François Godin <francois.h.godin@gmail.com>
2018-10-29 17:15:18 +0100

    Add files via upload

---
+++ Model.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConvertJsonEventsOfCalendarToIcsFile
+{
+    class Model
+    {
+        public class Event
+        {
+            public string ID { get; set; }
+            public DateTime DateEvent { get; set; }
+            public string Objet { get; set; }
+            public string Lieu { get; set; }
+            public string Str_Hdeb { get; set; }
+            public string Str_Hfin { get; set; }
+
+            public void Add(StringBuilder sb)
+            {
+                string DateFormat = "yyyyMMddTHHmmssZ";
+                string now = DateTime.Now.ToUniversalTime().ToString(DateFormat);
+                DateTime dtStart = this.DateEvent.Date + new TimeSpan(Convert.ToInt32(this.Str_Hdeb.Substring(0, 2)), 0, 0);
+                DateTime dtEnd = this.DateEvent.Date + new TimeSpan(Convert.ToInt32(this.Str_Hfin.Substring(0, 2)), 0, 0);
+
+                sb.AppendLine("BEGIN:VEVENT");
+                sb.AppendLine("DTSTART:" + dtStart.ToUniversalTime().ToString(DateFormat));
+                sb.AppendLine("DTEND:" + dtEnd.ToUniversalTime().ToString(DateFormat));
+                sb.AppendLine("DTSTAMP:" + now);
+                sb.AppendLine("UID:" + this.ID);
+                sb.AppendLine("CREATED:" + now);
+                sb.AppendLine("DESCRIPTION:" + "");
+                sb.AppendLine("LAST-MODIFIED:" + now);
+                sb.AppendLine("LOCATION:" + this.Lieu);
+                sb.AppendLine("SEQUENCE:0");
+                sb.AppendLine("STATUS:CONFIRMED");
+                sb.AppendLine("SUMMARY:" + this.Objet);
+                sb.AppendLine("TRANSP:OPAQUE");
+                sb.AppendLine("END:VEVENT");
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+using static ConvertJsonEventsOfCalendarToIcsFile.Model;
+
+namespace ConvertJsonEventsOfCalendarToIcsFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string distantJsonUrl = "http://srv-ipso.cloudapp.net/getevents";
+            try
+            {
+                var json = new WebClient().DownloadString(distantJsonUrl);
+                var listEvents = JsonConvert.DeserializeObject<List<Event>>(json);
+                 StringBuilder sb = WriteICallFormat(listEvents);
+                string path = AppDomain.CurrentDomain.BaseDirectory +  "events.ics";
+                System.IO.File.WriteAllText(path, sb.ToString());
+                Console.WriteLine("icall file succesfully saved at path : " + path);
+                Console.WriteLine(" \n please press enter for exit");
+            }
+            catch (WebException e)
+            {
+                Console.WriteLine("impossible d'atteindre l'url {0}, server exception msg : {1}", distantJsonUrl, e.Message);
+                Console.WriteLine(" \n please press enter for exit");
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("fatal error : ",e.Message);
+                 Console.WriteLine(" \n please press enter for exit");
+            }   
+            Console.ReadLine();
+        }
+
+        static StringBuilder WriteICallFormat(List<Event> events)
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine("BEGIN:VCALENDAR");
+            sb.AppendLine("PRODID:-//Fgodin//test");
+            sb.AppendLine("VERSION:2.0");
+            sb.AppendLine("METHOD:PUBLISH");
+
+            foreach (Event e in events)
+            {
+                e.Add(sb);
+            }
+            sb.AppendLine("END:VCALENDAR");
+            return sb;
+        }
+    }
+}
